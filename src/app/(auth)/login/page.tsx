c13afad3cac9e52c9f41b263b7dd815f8c1ed0b7
Abdullah <abdullahsarfraz636@gmail.com>
2025-12-13 14:40:34 +0500

    -- small changes --

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -297,7 +297,7 @@ export default function LoginPage() {
                             <div className="mt-8 text-center">
                                 <p className="text-slate-400">
                                     Don&apos;t have an account?{' '}
-                                    <button className="text-blue-400 hover:text-blue-300 font-bold transition-colors underline decoration-blue-400/50 hover:decoration-blue-300">
+                                    <button className="cursor-pointer text-blue-400 hover:text-blue-300 font-bold transition-colors underline decoration-blue-400/50 hover:decoration-blue-300">
                                         Create Account
                                     </button>
                                 </p>
